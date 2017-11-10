22aad12524429475a45d7896ae9007a2a7fe767c
seoyunho <yunho101775@gmail.com>
2017-11-11 00:49:30 +0900

    setInterval 30간격 company 정보 교체

--- company-view/src/Component/Contents/CompanyInfo.js
+++ company-view/src/Component/Contents/CompanyInfo.js
@@ -3,21 +3,20 @@ import './../main.css'
 
 class CompanyInfo extends React.Component{
     render(){
-        let summaryInfo=this.props.company.info;
-        let summaryTags="...";
+        let summaryInfo = this.props.company.info;
+        let summaryTags = null;
 
-        console.log(this.props.company.info.length);
-        if(this.props.company.info.length>117) summaryInfo=this.props.company.info.split("",117).join("")+"...";
-        
-        if(!!this.props.company.tags) summaryTags=this.props.company.tags.splice(0,3);
+        if (this.props.company.info.length > 117) summaryInfo = this.props.company.info.split("", 117).join("") + "...";
 
-        let getTags=()=>{
-            let i=0;
+        if (!!this.props.company.tags) summaryTags = this.props.company.tags.splice(0, 3);
+
+        let getTags = () => {
+            let i = 0;
             return (
-                summaryTags.map(function(tag){
+                summaryTags.map(function (tag) {
                     i++;
-                    if(i===3) return tag+"...";
-                    else return tag+",";
+                    if (i === summaryTags.length) return tag + "...";
+                    else return tag + ", ";
                 })
             )
         }
@@ -28,7 +27,7 @@ class CompanyInfo extends React.Component{
                 <div className="title_info">기업정보</div>
                 <h4 className="company_info">설립일 : {this.props.company.establish}</h4>
                 <h4 className="company_info">구성원 : {this.props.company.member_count}</h4>
-                <h3 className="company_major">{summaryTags}</h3>
+                <h3 className="company_major">{!!summaryTags ? getTags() : "..."}</h3>
                 <h3 className="company_intro">{summaryInfo}</h3>
             </div>   
         )
--- company-view/src/Component/Contents/CompanyWrapper.js
+++ company-view/src/Component/Contents/CompanyWrapper.js
@@ -2,6 +2,8 @@ import React from 'react';
 import CompanyCard from './CompanyCard';
 import './../main.css';
 import axios from 'axios';
+import logo from './logo.png';
+import img from './img.png';
 
 class CompanyWrapper extends React.Component{
 
@@ -10,12 +12,10 @@ class CompanyWrapper extends React.Component{
 
         this.state = {
             firstCompany: {
-                "address": "대한민국 서울특별시 강남구 학동로 128 5층",
-                "establish": "2015-05-22",
-                "image_url": "https://media.rocketpunch.com/company/8284/tradlinx_cover_1471500163.png",
+                "address": "전공동아리 그램",
+                "establish": "2017-03-08",
                 "info": "트레드링스는 2015년에 설립된 회사로 사원수 12명 규모의 벤처기업입니다.   2015년 08월 설립 3개월만에 Seed투자를 유치할만큼 가능성과시장성을동시에 인정받은 기업니다.2016년 08월 Series A VC투자 20억 투자 유치를 하였습니다.   트레드링스는 물류 온라인 정보 제공 및 플랫폼 서비스를 제공하는 회사입니다.  트레드링스는 비효율적인 국제물류 시장에 21세기형 온라인 물류 플랫폼을 제공함으로써  중소기업의 수출입 증대에 앞장서는 혁신적인 기업입니다.  \n\n더 보기",
                 "kind": "rocketpunch",
-                "logo_url": "https://media.rocketpunch.com/cache/fd/6b/fd6b1edc7a358ea4600158296c4e50f1.png",
                 "member_count": "11-50명",
                 "name": "트레드링스(TradLinx)",
                 "positions": [
@@ -31,30 +31,15 @@ class CompanyWrapper extends React.Component{
                   }
                 ],
                 "tags": [
-                  "모바일서비스",
-                  "웹서비스",
-                  "마케팅",
-                  "소프트웨어",
-                  "데이터",
-                  "IT서비스",
-                  "O2O",
-                  "platform",
-                  "BigData",
-                  "물류",
-                  "Contents",
-                  "logistics",
-                  "forwarder",
-                  "International",
-                  "Shipping"
+                  "안드로이드",
+                  "",
                 ]
               },
             secondCompany: {
                 "address": "대한민국 서울특별시 강남구 학동로 128 5층",
                 "establish": "2015-05-22",
-                "image_url": "https://media.rocketpunch.com/company/8284/tradlinx_cover_1471500163.png",
                 "info": "트레드링스는 2015년에 설립된 회사로 사원수 12명 규모의 벤처기업입니다.   2015년 08월 설립 3개월만에 Seed투자를 유치할만큼 가능성과시장성을동시에 인정받은 기업니다.2016년 08월 Series A VC투자 20억 투자 유치를 하였습니다.   트레드링스는 물류 온라인 정보 제공 및 플랫폼 서비스를 제공하는 회사입니다.  트레드링스는 비효율적인 국제물류 시장에 21세기형 온라인 물류 플랫폼을 제공함으로써  중소기업의 수출입 증대에 앞장서는 혁신적인 기업입니다.  \n\n더 보기",
                 "kind": "rocketpunch",
-                "logo_url": "https://media.rocketpunch.com/cache/fd/6b/fd6b1edc7a358ea4600158296c4e50f1.png",
                 "member_count": "11-50명",
                 "name": "트레드링스(TradLinx)",
                 "positions": [
@@ -129,15 +114,23 @@ class CompanyWrapper extends React.Component{
         };
     }
     componentDidMount (){
-        axios.get('http://52.79.134.200:3002/company')
-            .then(response => {this.setFirstCompany(response.data)});
-        axios.get('http://52.79.134.200:3002/company')
-            .then(response => {this.setSecondCompany(response.data)});
-        axios.get('http://52.79.134.200:3002/company')
-            .then(response => {this.setThirdCompany(response.data)});
+        // axios.get('http://52.79.134.200:3002/company')
+        //     .then(response => {this.setFirstCompany(response.data)});
+        // axios.get('http://52.79.134.200:3002/company')
+        //     .then(response => {this.setSecondCompany(response.data)});
+        // axios.get('http://52.79.134.200:3002/company')
+        //     .then(response => {this.setThirdCompany(response.data)});
+
+        setInterval(() => {
+            axios.get('http://52.79.134.200:3002/company')
+                .then(response => { this.setFirstCompany(response.data) });
+            axios.get('http://52.79.134.200:3002/company')
+                .then(response => { this.setSecondCompany(response.data) });
+            axios.get('http://52.79.134.200:3002/company')
+                .then(response => { this.setThirdCompany(response.data) });
+        }, 30000);
     }
-    setFirstCompany(data) {
-        console.log(data);
+    setFirstCompany(data) { 
         this.setState({
             ...this.state,
             firstCompany: data
@@ -160,8 +153,8 @@ class CompanyWrapper extends React.Component{
         console.log(this.state);
         return(
             <div id="content"> 
-                <CompanyCard img_url={this.state.firstCompany.image_url}
-                             logo_url={this.state.firstCompany.logo_url}
+                <CompanyCard img_url={this.state.firstCompany.image_url?this.state.firstCompany.image_url:img}
+                             logo_url={this.state.firstCompany.logo_url?this.state.firstCompany.logo_url:logo}
                              postion={this.state.firstCompany.postion}
                              company={this.state.firstCompany} />
                 <CompanyCard img_url={this.state.secondCompany.image_url}
